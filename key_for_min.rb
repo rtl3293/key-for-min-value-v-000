929512fea51e7d2c60a709e145b9b17776deaa0a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 21:02:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = 0
+  smallest = String.new
   if name_hash == {}
     nil
   else
