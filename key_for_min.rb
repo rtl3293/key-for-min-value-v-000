c90901e087dee9eeddba762b4195d8bec96f69ab
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 21:11:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,14 @@ def key_for_min_value(name_hash)
     end
     smallest = array_of_values[0]
     array_of_values.each do |value|
-
+      if value < smallest
+        smallest = value
+      end
+    name_hash.select do |key , value|
+      if value == smallest
+        smallest_key = key
+      end
+    end
     smallest_key
   end
 end
